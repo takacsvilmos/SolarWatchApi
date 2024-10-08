31a18275a7d50186912696d8545c75d683ffd2b6
takacsvilmos <vilmos.takacs@gmail.com>
2024-10-08 10:15:42 +0200

    CityCoordinatesApi refactored with IWebClient

--- SolarWatch/Services/CityCoordinatesApi.cs
+++ SolarWatch/Services/CityCoordinatesApi.cs
@@ -6,20 +6,21 @@ namespace SolarWatch.Services
     public class CityCoordinatesApi
     {
         private readonly ILogger<CityCoordinatesApi> _logger;
+        private readonly IWebClient _client;
 
-        public CityCoordinatesApi(ILogger<CityCoordinatesApi> logger)
+        public CityCoordinatesApi(ILogger<CityCoordinatesApi> logger, IWebClient client)
         {
             _logger = logger;
+            _client = client;
         }
 
         public string GetCityCoordinates(string city)
         {
             var apiKey = "498c6649b077e74b3bee6e05708d9bfb";
             var url = $"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={apiKey}";
-            using var client = new WebClient();
             
             _logger.LogInformation("Calling openWeather API with url: {url}", url);
-            return client.DownloadString(url);
+            return _client.DownloadString(url);
         }
     }
 }
