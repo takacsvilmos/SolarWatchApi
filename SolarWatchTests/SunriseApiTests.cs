c107e229c4c69e42034599ab6403c4798c369b24
takacsvilmos <vilmos.takacs@gmail.com>
2024-10-08 11:15:25 +0200

    ControllerSuccesCaseTest pass

--- SolarWatchTests/SunriseApiTests.cs
+++ SolarWatchTests/SunriseApiTests.cs
@@ -1,7 +1,10 @@
 using System.Net;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SolarWatch;
+using SolarWatch.Controllers;
 using SolarWatch.Services;
 
 namespace SolarWatchTests
@@ -42,6 +45,47 @@ namespace SolarWatchTests
 
             var result = service.GetCityCoordinates(city);
             Assert.AreEqual(expectedResponse, result);
+        }
+
+        [Test]
+        public void SunsetSunriseController_SuccessCase()
+        {
+            var city = "Washington";
+            var coordinates= "{latitude:47.4979, longitude: 19.0402 }";
+            var mockWebClient = new Mock<IWebClient>();
+            mockWebClient.Setup(client => client.DownloadString(It.IsAny<string>()))
+                .Returns(coordinates);
+            var mockLogger = new Mock<ILogger<CityCoordinatesApi>>();
+            var cityCoordinatesService = new CityCoordinatesApi(mockLogger.Object, mockWebClient.Object);
+
+
+            var expectedResponse = "{\"sunrise\":\"6:30:00 AM\", \"sunset\":\"7:30:00 PM\"}";
+            var latitude =  47.4979;
+            var longitude = 19.0402;
+            var date = new DateOnly(2023, 10, 1);
+            var mockSunsetWebClient = new Mock<IWebClient>();
+            mockSunsetWebClient.Setup(client => client.DownloadString(It.IsAny<string>()))
+                .Returns(expectedResponse);
+            var mockSunriseSunsetLogger = new Mock<ILogger<SunriseSunsetApi>>();
+            var sunsetSunriseService = new SunriseSunsetApi(mockSunriseSunsetLogger.Object, mockSunsetWebClient.Object);
+
+            var mockJsonProcessor = new Mock<IJsonProcessor>();
+            mockJsonProcessor.Setup(processor => processor.Process(It.IsAny<string>()))
+                .Returns(new CityCoordinates(latitude, longitude));
+            mockJsonProcessor.Setup(processor => processor.MakeSunriseSunsetTime(It.IsAny<string>()))
+                .Returns(new SunriseSunsetTime("6:30:00 AM", "7:30:00 PM"));
+
+            var controller = new SunsetSunriseController(new Mock<ILogger<SunsetSunriseController>>().Object,
+                cityCoordinatesService, mockJsonProcessor.Object, sunsetSunriseService);
+
+            var result = controller.Get(city, date.Year, date.Month, date.Day);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+
+            var responseData = okResult.Value as SunriseSunsetData;
+
+            Assert.AreEqual("6:30:00 AM", responseData.Sunrise);
+            Assert.AreEqual("7:30:00 PM", responseData.Sunset);
         }
     }
 }
