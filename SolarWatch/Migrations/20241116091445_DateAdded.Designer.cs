69a7f88900795cc7c61c61f10cdc3de81e6a9a8d
takacsvilmos <vilmos.takacs@gmail.com>
2024-11-16 10:16:09 +0100

    Date migrated to db

--- SolarWatch/Migrations/20241116091445_DateAdded.Designer.cs
+++ SolarWatch/Migrations/20241116091445_DateAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SolarWatch.Data;
 
@@ -11,9 +12,11 @@ using SolarWatch.Data;
 namespace SolarWatch.Migrations
 {
     [DbContext(typeof(SolarwatchDbContext))]
-    partial class SolarwatchDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241116091445_DateAdded")]
+    partial class DateAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
